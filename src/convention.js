cc43a9df8128fedc8a06df3c49a25db17dba693b
happyGyu <happyboyto.dev@gmail.com>
2022-05-11 16:24:14 +0900

    Build: 빌드 파일 경로 수정

--- src/convention.js
+++
@@ -1,7 +0,0 @@
-export const activityType = {
-  PURCHASE: "purchase",
-  OUT_OF_STOCK: "outOfStock",
-  LACK_OF_MONEY: "lackOfMoney",
-  INPUT_MONEY: "inputMoney",
-  RETURN_MONEY: "returnMoney"
-};
--- src/fetcher.js
+++
@@ -1,7 +0,0 @@
-export const fetchData = async (target) => {
-  return await (await fetch(getRequestURL(target))).json();
-};
-
-const getRequestURL = (target) => {
-  return `http://localhost:3000/mocks/${target}.json`;
-};
